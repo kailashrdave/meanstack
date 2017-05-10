a193d2967d58ff389d8bd308c6d4a753f1f08607
kailashrdave <kailashrdave@gmail.com>
2017-05-11 01:00:58 +0200

    REST api with mongo
    REST api with mongo

--- app.js
+++ app.js
@@ -6,6 +6,23 @@ var port = 8000;
 app.use(express.static('public'));
 app.use(express.static('src/views'));
 
+
+ mongoose = require('mongoose'),
+  Task = require('./api/models/task'),
+  //bodyParser = require('body-parser');
+  
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/Tododb'); 
+
+
+//app.use(bodyParser.urlencoded({ extended: true }));
+//app.use(bodyParser.json());
+
+
+var routes = require('./routes/taskroutes');
+routes(app);
+
+
 app.get('/',function(req, res){
 		res.send('hello world');
 });
