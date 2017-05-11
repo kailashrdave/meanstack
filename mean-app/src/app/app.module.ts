3ed5a55fe1c005c7e9f6a5c9a104c1690dfd07a2
kailashrdave <kailashrdave@gmail.com>
2017-05-12 01:21:15 +0200

    added Angular

---
+++ mean-app/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { TasksComponent } from './tasks/tasks.component';
+
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TasksComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
